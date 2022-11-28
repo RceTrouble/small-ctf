4b2f4d45f10afc89983b548e4597a3f1de78930d
rce <118572559+RceTrouble@users.noreply.github.com>
2022-11-28 09:10:17 +0000

    Update db.php

--- ctf/app/db.php
+++ ctf/app/db.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost","hacker","holy!shit!","secure_users");
+    $con = mysqli_connect("localhost","hacker","s3cur3!p4ss!","test_users");
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
